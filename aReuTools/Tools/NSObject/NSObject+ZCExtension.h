e2266e24af3a1315613ee17e81ee9451ac59acfa
aReu <aReu@aReu-2.local>
2018-01-18 12:45:18 +0800

    no message

---
+++ aReuTools/Tools/NSObject/NSObject+ZCExtension.h
@@ -0,0 +1,23 @@
+//
+//  NSObject+ZCExtension.h
+//  aReuTools
+//
+//  Created by aReu on 2018/1/18.
+//  Copyright © 2018年 aReu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject(ZCExtension)
+
+
+@end
+
+
+
+@interface NSObject(ZCParse)
+
++ (id)setParse:(id)Json;
+
+@end
+
---
+++ aReuTools/Tools/NSObject/NSObject+ZCExtension.m
@@ -0,0 +1,19 @@
+//
+//  NSObject+ZCExtension.m
+//  aReuTools
+//
+//  Created by aReu on 2018/1/18.
+//  Copyright © 2018年 aReu. All rights reserved.
+//
+
+#import "NSObject+ZCExtension.h"
+#import "YYModel.h"
+
+@implementation NSObject(ZCExtension)
+
+@end
+
+
+@implementation NSObject(ZCParse)
+
+@end
